3e25505643a405eb6daed58c82a6dd915fb07969
kit0ra <80955239+kit0ra@users.noreply.github.com>
2024-02-13 14:29:24 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// toggle dark/light mode logic
+const toggleTheme = () => {
+  const root = document.documentElement
+  const currentTheme = root.getAttribute('data-theme')
+  const themeIcon = document.querySelector('#toggle-button')
+  themeIcon.classList.toggle('fa-moon')
+  root.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark')
+}
+
+const applyUserPreferredMode = () => {
+  const root = document.documentElement
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches
+
+  // Apply user's preferred mode
+  root.setAttribute('data-theme', prefersDarkMode ? 'dark' : 'light')
+}
+
+const toggleButton = document.querySelector('#toggle-button')
+window.addEventListener('load', applyUserPreferredMode)
+toggleButton.addEventListener('click', toggleTheme)
+
+// --------------------------------------------------------------
+// close/open sidebar  logic
+function collaplseSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.classList.toggle('collapsed')
+
+  const sidebarHeader = document.querySelector('.sidebar-header')
+  const children = [...sidebarHeader.children]
+  const childrenToHide = children.slice(1)
+  if (sidebar.classList.contains('collapsed')) {
+    childrenToHide.forEach(child => hideElement(child))
+    collapseMenu()
+  } else {
+    childrenToHide.forEach(child => showElement(child))
+    expanedMenu()
+  }
+  const sidebarLinks = document.querySelector('.sidebar-links')
+  sidebarLinks.classList.toggle('collapsed-links')
+
+  const content = document.querySelector('.content')
+  content.classList.toggle('expanded-content')
+}
+
+function hideElement(element) {
+  element.classList.add('hidden')
+}
+
+function showElement(element) {
+  element.classList.remove('hidden')
+}
+
+function collapseMenu() {
+  const sidebarMenuItems = [...document.querySelectorAll('.sidebar-menu__item')]
+  sidebarMenuItems.forEach(item => {
+    item.classList.add('collapsed-item')
+    item.firstElementChild.classList.add('collapsed-icon')
+  })
+}
+
+function expanedMenu() {
+  const sidebarMenuItems = [...document.querySelectorAll('.sidebar-menu__item')]
+  sidebarMenuItems.forEach(item => {
+    item.classList.remove('collapsed-item')
+    item.firstElementChild.classList.add('collapsed-icon')
+  })
+}
+
+const hamburgerButton = document
+  .querySelector('.hamburger')
+  .addEventListener('click', collaplseSidebar)
