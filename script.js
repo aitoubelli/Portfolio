6e1b065875f7840d184a5031a3dee9569db4c0a3
kit0ra <80955239+kit0ra@users.noreply.github.com>
2024-02-14 21:18:46 +0100

    add particles.js

--- script.js
+++ script.js
@@ -169,3 +169,106 @@ function deletePhrase() {
 
 typePhrase() // start the typing effect
 // --------------------------------------------------------------
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: '#e94560', // Particle color
+    },
+    shape: {
+      type: 'circle', // Can be "circle", "edge", "triangle", "polygon", "star", "image"
+      stroke: {
+        width: 0,
+        color: '#e94560',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#e94560', // Line color
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none', // Can be "none", "top", "top-right", "right", "bottom-right", "bottom", "bottom-left", "left", "top-left"
+      random: false,
+      straight: false,
+      out_mode: 'out', // Can be "out" or "bounce"
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse', // Can be "grab", "bubble", "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: 'push', // Can be "push", "remove", "bubble", "repulse"
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+})
